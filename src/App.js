5144269922cca498275a4dbe69a4b1adc756ddb1
Keshav <keshavkumar14092001@gmail.com>
2022-08-04 20:32:09 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { UilSearch } from "@iconscout/react-unicons";
+import TimeAndLocation from "./components/TimeAndLocation.jsx";
+import TemperatureAndDetails from "./components/TemperatureAndDetails.jsx";
+import Spinner from "./components/Spinner";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+let num;
+
+const App = () => {
+    const [city, setCity] = useState("");
+    const [weather, setWeather] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.REACT_APP_WEATHER_KEY}`;
+    let thresHold = 0;
+
+    const fetchWeather = (e) => {
+        e.preventDefault();
+        setLoading(true);
+        axios
+            .get(url)
+            .then((response) => {
+                setWeather(response.data);
+                thresHold = ((response.data.main.temp - 32) * 5) / 9;
+                formatBackground(thresHold);
+                console.log(thresHold);
+                console.log(response.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        setCity("");
+        setLoading(false);
+    };
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        fetchWeather(e);
+    };
+
+    const formatBackground = (n) => {
+        if (n) {
+            num = n;
+        }
+        if (num > 28) {
+            return "from-yellow-700 to-orange-700";
+        } else {
+            return "from-cyan-700 to-blue-700";
+        }
+    };
+
+    if (loading) {
+        return <Spinner />;
+    } else {
+        return (
+            <div className="App">
+                <div
+                    className={`mx-auto max-w-screen-md mt-20 mb-6 py-5 px-32 bg-gradient-to-br h-fit shadow-xl shadow-gray-800 rounded-md ${formatBackground(0)}`}
+                >
+                    <div className="flex flex-row justify-center my-6">
+                        <div className="flex flex-row w-3/4 justify-center items-center space-x-4">
+                            <form className="w-[100%]" onSubmit={(e) => fetchWeather(e)}>
+                                <input
+                                    type="text"
+                                    className="text-xl font-light p-2 w-full shadow-xl rounded-md capitalize placeholder:lowercase focus:outline-none"
+                                    placeholder="!!! Search for city !!!"
+                                    onChange={(e) => setCity(e.target.value)}
+                                />
+                            </form>
+                            <UilSearch
+                                size={25}
+                                className="text-white cursor-pointer transition ease-out hover:scale-125"
+                                onClick={(e) => handleClick(e)}
+                            />
+                        </div>
+                    </div>
+                    {weather.main && <TimeAndLocation data={weather} />}
+                    {weather.main && <TemperatureAndDetails data={weather} />}
+                </div>
+            </div>
+        );
+    }
+};
 
 export default App;
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        <>
+            <img className='w-[200px] m-auto block' src={'/spinner.gif'} alt='loading' />
+        </>
+    )
+}
+
+export default Spinner;
